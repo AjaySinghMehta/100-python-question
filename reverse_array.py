60149d5867a19407d69c9638eae4d4eb4b906075
Ajay Singh Mehta <ajay.mehta332005@gmail.com>
2024-09-10 21:53:41 +0530

    twenty eighth commit

---
+++ reverse_array.py
@@ -0,0 +1,24 @@
+'''
+we are given an array we need to reverse it 
+ex = array =  [1,2,3,4] ,  result = [4,3,2,1] 
+
+there are three ways to do this 
+first one is for python only 
+
+'''
+
+# method 1 
+
+arr = list(map(int, input('enter array : ').split()))
+result = arr[::-1]
+print('first method output (using array[::-1]): ',result)
+
+
+# second method :
+for i in range(len(arr)//2):
+    arr[i] = arr[i]+arr[len(arr)-1]
+    arr[len(arr)-1] = arr[i] -  arr[len(arr)-1]
+    arr[i] = arr[i] -  arr[len(arr)-1]
+    
+print('using second method where we swap compliment elements : ',arr)
+    
